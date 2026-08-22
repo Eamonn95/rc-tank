58202e90452b4398462a508f1d965e96a93c0c4d
Eamonn95 <90157070+Eamonn95@users.noreply.github.com>
2026-08-22 16:37:18 -0400

    Add Wi-Fi secrets template

---
+++ firmware/mkr_wifi_motor_control/arduino_secrets.example.h
@@ -0,0 +1,5 @@
+// Copy this file to arduino_secrets.h and fill in your Wi-Fi details.
+// Do NOT commit arduino_secrets.h to GitHub.
+
+#define SECRET_SSID "YOUR_WIFI_NAME"
+#define SECRET_PASS "YOUR_WIFI_PASSWORD"
